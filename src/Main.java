a385c4bde838b30b1a0a245201b69a440f9bd2ed
emrekabakci <emrekabakci.2@icloud.com>
2023-12-15 22:33:32 +0300

    Comment added.

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Year variable defined.
         int year;
 
+        //New scanner created.
         Scanner inp = new Scanner(System.in);
 
+        //Data retrieved from user.
         System.out.print("Please enter the year : ");
         year = inp.nextInt();
 
+        //Leap year calculated and printed.
         if (year % 100 == 0){
 
             if (year % 400 == 0){
